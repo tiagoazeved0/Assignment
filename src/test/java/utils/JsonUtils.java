8c22d985fe2eb43bac5f50f9174e8ecf8ac833aa
MINA-AZEVEDO Tiago <tiago.mina-azevedo@vinci-energies.net>
2022-09-17 19:10:48 +0100

    Initial commit

---
+++ src/test/java/utils/JsonUtils.java
@@ -0,0 +1,24 @@
+package utils;//package utils;
+//
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.ParseException;
+//
+//public class JsonUtils {
+//
+//    public static class Json{
+//
+//        static String ReadUrlFromJson(String key) {
+//
+//            JSONParser jsonParser = new JSONParser();
+//
+//            jsonParser.
+//
+//
+//            if (key.contains("homepage"))
+//            return "";
+//        }
+//    }
+//
+//}
